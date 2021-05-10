f8ad2e37335addbb731e2c558cc9822a380f1d98
Vishal Panigrahy <vishal.panigrahy@kellogg.northwestern.edu>
2021-05-10 09:40:17 -0500

    Hw6 v1

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -22,26 +22,44 @@ exports.handler = async function(event) {
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
   
+  // check if both the year and genre parameters are provided
   if (year == undefined || genre == undefined) {
+    // if not provided, show customized error message to avoid errors in future 
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `ERROR! Please input movie year and genre parameters in the API. eg: ?year=xyz&genre=Abc. Year range: 2010-2020. Genres: Action,Adventure,Animation,Biography,Comedy,Crime,Documentary,Drama,Family,Fantasy,History,Horror,Music,Musical,Mystery,News,Reality-TV,Romance,Sci-Fi,Sport,Talk-Show,Thriller,War,Western` // a string of data
     }
   }
+  // if both the parameters are provided, filter the movies list
   else {
+    // create the return value with 2 data points - no of results, movies list
     let returnValue = {
       numResults: 0,
       movies: []
     }
-
+    // iterate through the movies database  
     for (let i=0; i < moviesFromCsv.length; i++) {
-
+      let movie = moviesFromCsv[i] // store each movie row in memory 
+      // include movie results if movie release year and genre match the inputs
+      if (movie.startYear == year && movie.genres.includes(genre) == true) { 
+        // exlcude movie results if genres or runtime is \NN
+        if (movie.genres != `\\N` && movie.runtimeMinutes != `\\N`){
+          // create an object and store details of the movie in it 
+          let postObject = {
+            primaryTitle: movie.primaryTitle,
+            year: movie.startYear,
+            genres: movie.genres
+          }
+          // add the object to the movies array in returnValue
+          returnValue.movies.push(postObject) 
+        }
+      }
     }
-
+    returnValue.numResults = returnValue.movies.length // add the no of results to returnValue
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(returnValue) // convert returnValue to a string of data
     }
   }
 }
